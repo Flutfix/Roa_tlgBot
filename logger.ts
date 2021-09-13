c07dbbe5bcdb0a041961be9937aa0a3d29950c60
sithell <thesithell@gmail.com>
2021-09-14 00:12:37 +0300

    Add : after INFO in logger

--- logger.ts
+++ logger.ts
@@ -7,17 +7,17 @@ function log(msg: string, options: Array<any> = []) {
 
 function error(msg: string, options: Array<any> = []) {
     const date = helpers.formatDate(new Date());
-    console.error(helpers.inject("[${0}] ERROR ${1}", [date, msg]), options);
+    console.error(helpers.inject("[${0}] ERROR: ${1}", [date, msg]), options);
 }
 
 function info(msg: string, options: Array<any> = []) {
     const date = helpers.formatDate(new Date());
-    console.log(helpers.inject("[${0}] INFO ${1}", [date, msg]), options);
+    console.log(helpers.inject("[${0}] INFO: ${1}", [date, msg]), options);
 }
 
 function warn(msg: string, options: Array<any> = []) {
     const date = helpers.formatDate(new Date());
-    console.log(helpers.inject("[${0}] WARN ${1}", [date, msg]), options);
+    console.log(helpers.inject("[${0}] WARN: ${1}", [date, msg]), options);
 }
 
 module.exports = {
